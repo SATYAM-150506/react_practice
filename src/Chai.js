e2ddbba08a39b68dab7f2fdf9143548e6cda07b8
SATYAM-150506 <satyampruthi2006@gmail.com>
2025-09-13 01:47:01 +0530

    Initial Commit

---
+++ src/Chai.js
@@ -0,0 +1,8 @@
+//the naming of file should be in capital letter for better practice but it should also work with small letter
+//but function name should be in capital letter but in case of vite strict rules should be followed for camel case
+function Chai() {
+    return(
+        <h1>Code from Chai.js</h1>
+    )
+}
+export default Chai
